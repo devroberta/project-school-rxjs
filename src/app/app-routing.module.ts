d8f23d5cae0ef16031563be9638b09f2eb880a7c
Roberta de Siqueira <designer.roberta@gmail.com>
2024-04-26 14:30:46 -0300

    partial implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule) },
+  { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'courses', loadChildren: () => import('./pages/courses/courses.module').then(m => m.CoursesModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
